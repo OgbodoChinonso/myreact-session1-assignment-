0828de31449a62ca8c5abcb5ffae9426412df222
pascal <ogbodopaschal@gmail.com>
2025-12-04 07:13:36 +0100

    Exercise 2 cmpleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //import { useState } from 'react'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage';
+import AboutReact from './components/AboutReact'
 
 function App() {
 
   return (
     <>
     <WelcomeMessage />
+    <AboutReact />
     </>
   )
 }
---
+++ src/components/AboutReact.tsx
@@ -0,0 +1,12 @@
+function AboutReact(){
+  return (
+    <>
+    <p>React is an external library</p>
+    <p>Jordan Walke created React</p>
+    <p>React was created in the year 2011</p>
+    <p>According to google, React is classified as a library because of its focused functionality and the level of control it provides to developers, which contrasts with the more comprehensive and opinionated nature of a framework.</p>
+    </>
+  )
+};
+
+export default AboutReact
