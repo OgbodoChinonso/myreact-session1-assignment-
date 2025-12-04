88ed0bccd64e02d335d3502daf57b17e7f389cb3
pascal <ogbodopaschal@gmail.com>
2025-12-04 07:02:33 +0100

    Exercise 1 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 //import { useState } from 'react'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
 
   return (
     <>
-    
+    <WelcomeMessage />
     </>
   )
 }
---
+++ src/components/WelcomeMessage.tsx
@@ -0,0 +1,9 @@
+function WelcomeMessage(){
+  return(
+    <>
+    <h3>Welcome to React!</h3>
+    </>
+  )
+};
+
+export default WelcomeMessage;
